1d2447b438a3b3541ad1aca89c6fd3837997f489
Dani5822 <75309763+Dani5822@users.noreply.github.com>
2024-10-03 14:36:29 +0200

    errors,post methode

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { newaccountdto } from './newAccount.dto';
+import { Response } from 'express';
+import { error } from 'console';
 
 @Controller()
 export class AppController {
@@ -11,5 +14,64 @@ export class AppController {
     return {
       message: this.appService.getHello()
     };
+  }
+
+  #accounts=[
+    {
+      id:'1234-5678',
+      owner:'Admin',
+      balance:15000,
+    },
+    {
+      id:'2222-3333',
+      owner:'Józsi',
+      balance:10000,
+    },
+    {
+      id:'8888-6666',
+      owner:'Me',
+      balance:150000,
+    },
+  ]
+
+  @Get('newaccount')
+  @Render('newaccountform')
+  newaccountform(){
+    return{
+      error:[],
+      data:{}
+
+    }
+  }
+
+  @Post('newaccount')
+  newaccount(@Body() accountdata: newaccountdto,@Res() response:Response){
+    const errors:string[]=[]
+    if(!accountdata.balance||!accountdata.id||!accountdata.owner){
+      errors.push("Minden mezőt kötelező megadni!")
+    }
+    let newaccount={
+      id:accountdata.id,
+      owner:accountdata.owner,
+      balance: parseInt(accountdata.balance)
+    }
+    if(errors.length>0){
+      response.render("newaccountform",{
+        errors,
+        data:accountdata
+      })
+    }
+
+    this.#accounts.push(newaccount)
+    response.redirect(303,'/newaccountsuccess')
+   
+  }
+
+  @Get('newaccountsuccess')
+  @Render('success')
+  newaccountsuccess(){
+    return{
+      accounts:this.#accounts.length
+    }
   }
 }
---
+++ src/newAccount.dto.ts
@@ -0,0 +1,6 @@
+export class newaccountdto{
+    id:string;
+    owner:string;
+    balance:string;
+    
+}
